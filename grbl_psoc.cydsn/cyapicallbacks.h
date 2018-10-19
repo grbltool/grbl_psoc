2b561599d663cb487d2be0a6ba5cbc427955e8aa
Andreas J <andreas@jungierek.de>
2018-10-19 17:31:43 +0200

    first (half) working attempt
    - all command line inputs have a response
    - playing a gcode file stops before first move, maybe interrupts interferring?

--- grbl_psoc.cydsn/cyapicallbacks.h
+++ grbl_psoc.cydsn/cyapicallbacks.h
@@ -15,6 +15,16 @@
 
     /*Define your macro callbacks here */
     /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
+  
+    #include "..\..\..\GIT\grbl_1_1_g_psoc\grbl\config.h"
+
+    #ifdef PSOC_USB
+      #define USBUART_EP_3_ISR_ENTRY_CALLBACK
+      #ifdef USBUART_EP_3_ISR_ENTRY_CALLBACK
+        #include "..\..\..\GIT\grbl_1_1_g_psoc\grbl\serial.h"
+        #define USBUART_EP_3_ISR_EntryCallback isr_rx_handler
+      #endif
+    #endif
 
     
 #endif /* CYAPICALLBACKS_H */   
